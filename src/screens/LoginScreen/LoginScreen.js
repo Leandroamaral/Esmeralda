ca59e3339d11c7c66f07eccf2cd11bbad64e031a
CodeCommit-at-974284182788 <CodeCommit-at-974284182788>
2022-12-22 11:05:39 -0300

    modified:   src/screens/LoginScreen/LoginScreen.js 	modified:   src/screens/MainScreen/MainScreen.js

--- src/screens/LoginScreen/LoginScreen.js
+++ src/screens/LoginScreen/LoginScreen.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import { Image, Text, TextInput, TouchableOpacity, View } from 'react-native'
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 
-import {LinearGradient} from 'expo-linear-gradient'
+import { LinearGradient } from 'expo-linear-gradient'
 import { firebase } from '../../firebase/config'
 
 //Authetication Social
