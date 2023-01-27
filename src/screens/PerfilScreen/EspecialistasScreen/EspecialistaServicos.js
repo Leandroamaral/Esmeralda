e002870c9901633b6e0c77b9caaa0c2a93bf67cc
Leandro <leobsb@yahoo.com>
2023-01-27 15:01:01 -0300

    EspecialistaServicos 	modified:   src/screens/PerfilScreen/EspecialistasScreen/EspecialistaServicos.js

--- src/screens/PerfilScreen/EspecialistasScreen/EspecialistaServicos.js
+++ src/screens/PerfilScreen/EspecialistasScreen/EspecialistaServicos.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react'
-import { Text, View, SafeAreaView, ScrollView, TouchableOpacity, TextInput } from 'react-native';
+import React, { useState, useEffect, useRef } from 'react'
+import { Text, View, SafeAreaView, ScrollView, TouchableOpacity, TextInput, RefreshControl, ActivityIndicator } from 'react-native';
 import { Picker } from '@react-native-picker/picker';
 
 import { AntDesign } from '@expo/vector-icons';
@@ -9,38 +9,13 @@ import styles from './styles';
 import { db } from '../../../firebase/config';
 import { Icones } from '../../FeedScreen/icons';
 
-const PersonCard = ({dados, navigation}) => {
-  
-  return (
-    <View style={styles.userCard}>
-      <Icones tipo={dados.Icone} width={45} height={45} fill="#92a494"  />
-        <View style={styles.userCardDescription}>
-        <Text style={{ marginLeft: 40, fontWeight: 'bold' }}>Serviço: {dados.Nome}</Text>
-        <View style={{marginLeft: 30, flexDirection: 'row' }}><Text style={{ marginLeft: 10, fontWeight: 'bold' }}>Tempo:</Text><Text style={{ marginLeft: 10}}>{dados.Tempo}</Text></View>
-        <View style={{marginLeft: 30,flexDirection: 'row'}}><Text style={{ marginLeft: 10, fontWeight: 'bold' }}>Valor:</Text><Text style={{ marginLeft: 20}}>{dados.Valor}</Text></View>
-        <View style={styles.userActions}>
-          <TouchableOpacity 
-            style={styles.editButton}
-          >
-            <AntDesign name="form" size={26} color='#1d817e'style={styles.padding10}/>
-          </TouchableOpacity>
-          <TouchableOpacity 
-            style={styles.editButton}
-          >
-            <AntDesign name="delete" size={26} color='#1d817e' style={styles.padding10}/>
-          </TouchableOpacity>
-        </View>
-      </View>
-    </View>  
-    )
-};
-
 
 export default function EspecialistaServicos ({ route, navigation }) {
 
-  const [servicos,setServicos] = useState([]);
   const [parametros, setParametros] = useState(route.params);
-  const [tempKey, setTempKey] = useState(0);
+  const [refreshing, setRefreshing] = useState(false);
+
+  const [dbservicos,setDBServicos] = useState([]);
   const [allServicos, setAllServicos] = useState([]);
 
   const [idservicos,setIdServicos] = useState();
@@ -48,80 +23,168 @@ export default function EspecialistaServicos ({ route, navigation }) {
   const [valor,setValor] = useState();
 
   useEffect(() => {
+    loadUserData();
+  }, [navigation]);
+  
+  const loadUserData = () => {
+    db
+    .collection('Servico')
+    .get()
+    .then(snapshot => {
+      setAllServicos (snapshot.docs.map(doc => {
+        const data = doc.data();
+        const id = doc.id;
+        return { id, ...data }
+      }))
+
+    })
+
+    if (parametros.itemId) {
       db
-        .collection('Servico')
+        .collection('Especialista')
+        .doc(parametros.itemId)
         .get()
         .then(snapshot => {
-          setAllServicos (snapshot.docs.map(doc => {
-            const data = doc.data();
-            const id = doc.id;
-            return { id, ...data }
-          }))
-
+            const shotdata = snapshot.data();
+            if(typeof(shotdata.Servicos) !== 'undefined') {
+              setDBServicos(shotdata.Servicos)
+            }
         })
+        .catch((e) => console.error)
+    }
 
-        if (parametros.itemId) {
-          db
-            .collection('Especialista')
-            .doc(parametros.itemId)
-            .get()
-            .then(snapshot => {
-                const shotdata = snapshot.data();
-                if(typeof(shotdata.Servicos) !== 'undefined') {
-                  setServicos(shotdata.Servicos)
-                  
-                  
-                }
-            })
-            .catch((e) => console.error)
-        }
 
-  }, []);
 
- 
-  function onSalvarServico() {
-    const temp1 = [{
-      idServicos: idservicos,
-      Valor: valor,
-      Tempo: tempo
-    }]
 
-    const temp2 = temp1.concat(servicos)
+  }
 
+
+  function onDeleteServico(id) {
+
+    const check = dbservicos.findIndex((el) => el.idServicos == id);
+    const x = dbservicos.splice(check,1);
     db
       .collection('Especialista')
       .doc(parametros.itemId)
       .update({
-        Servicos: temp2
+          Servicos: dbservicos
       })
       .then(() => {
-          alert('Atualização Efetuada');
+          alert('Registro apagado');
+          loadUserData();
       })
       .catch(() => {
           console.error(e);
       })
-      .finally(() => {
-          setTempKey(tempKey+1);
-      })
 
   }
 
-  const servicofinal = servicos.map((item) => ({...item, ...allServicos.find(itemf => item.idServicos == itemf.id) }))
+  function onEditServico(id) {
+    const selectservico = dbservicos.find((item) => item.idServicos == id);
+    const check = dbservicos.findIndex((el) => el.idServicos == id);
+    const x = dbservicos.splice(check,1);
+ 
+    setIdServicos(selectservico.idServicos);
+    setTempo(selectservico.Tempo);
+    setValor(selectservico.Valor);
+    
+    scrollViewRef.current.scrollToEnd();
+    //setComboServicos(comboServicos.concat(addcombo));
+ 
+  }
+ 
+  function onSalvarServico() {
+
+    if (idservicos && valor && tempo) {
+      const check = dbservicos.findIndex((el) => el.idServicos == idservicos);
+      var temp2 = dbservicos;
+
+      if (check > -1) {
+        dbservicos[check].Valor = valor;
+        dbservicos[check].Tempo = tempo;
+        temp2 = dbservicos;
+      } else {
+        const temp1 = [{
+          idServicos: idservicos,
+          Valor: valor,
+          Tempo: tempo
+        }]
+        temp2 = temp1.concat(dbservicos)
+      }
+      
+      db
+        .collection('Especialista')
+        .doc(parametros.itemId)
+        .update({
+          Servicos: temp2
+        })
+        .then(() => {
+            alert('Atualização Efetuada');
+            loadUserData();
+            setIdServicos('');
+            setTempo('');
+            setValor('');
+        })
+        .catch(() => {
+            console.error(e);
+        })
+    } else {
+      alert('Todos os campos são obrigatórios')
+    }
+  }
+
+
+
+  const PersonCard = ({dados, navigation}) => {
   
+    return (
+      <View style={styles.userCard}>
+        <Icones tipo={dados.Icone} width={45} height={45} fill="#92a494"  />
+          <View style={{width: 150}}>
+            <Text style={{ marginLeft: 10, fontSize: 18 }}>{dados.Nome}</Text>
+            <View style={{ marginLeft: 0, flexDirection: 'row' }}><Text style={{ marginLeft: 10, fontWeight: 'bold' }}>Tempo:</Text><Text style={{ marginLeft: 10}}>{dados.Tempo}</Text></View>
+            <View style={{ marginLeft: 1,flexDirection: 'row'}}><Text style={{ marginLeft: 10, fontWeight: 'bold' }}>Valor:</Text><Text style={{ marginLeft: 20}}>{dados.Valor}</Text></View>
+          </View>
+            <View style={styles.userActions}>
+              <TouchableOpacity 
+                style={styles.editButton}
+                onPress={() => onEditServico(dados.id)}
+              >
+                <AntDesign name="form" size={26} color='#1d817e'style={styles.padding10}/>
+              </TouchableOpacity>
+              <TouchableOpacity 
+                style={styles.editButton}
+                onPress={() => onDeleteServico(dados.id)}
+              >
+                <AntDesign name="delete" size={26} color='#1d817e' style={styles.padding10}/>
+              </TouchableOpacity>
+            </View>
+        
+      </View>  
+      )
+  };
+  
+  const listaservicos = dbservicos.map((item) => ({...item, ...allServicos.find(itemf => item.idServicos == itemf.id) }));
+  const comboServicos = allServicos.filter((item) => {return(!dbservicos.find((x) => {return(x.idServicos == item.id)}))});
+  const scrollViewRef = useRef();
+    
   return(
     <SafeAreaView>
-      <ScrollView>
-        <View key={tempKey.toString}>
-        {servicofinal.map((item,key)=>(
+      {refreshing ? <ActivityIndicator /> : null}
+      <ScrollView 
+        refreshControl={<RefreshControl refreshing={refreshing} onRefresh={loadUserData} />}
+        ref={scrollViewRef}
+      >
+        <View>
+        {listaservicos.map((item,key)=>(
             <PersonCard 
               key={key}
               navigation={navigation}
               dados={item}
-
             />
         ))}
         </View>
-        <View style={{alignItems: 'center', padding:10}}>
+        <View style={{alignItems: 'center', padding:10, marginTop: 20}}>
           <Text style={{fontSize: 15, fontWeight: 'bold'}}>Adicionar Novo Serviço</Text>
           <Picker style={styles.input} 
             mode="dropdown"
@@ -129,7 +192,7 @@ export default function EspecialistaServicos ({ route, navigation }) {
             onValueChange={(item) => setIdServicos(item)}
           >
             <Picker.Item label="Selecione um serviço" style={{color:'#AAA'}} />
-            {allServicos.map((item,index) => { return(
+            {comboServicos.map((item,index) => { return(
               <Picker.Item label={item.Nome} value={item.id} key={index} />
             )})}
           </Picker>
@@ -160,7 +223,8 @@ export default function EspecialistaServicos ({ route, navigation }) {
             onChangeText={(item) => setValor(item)}
           />
           <View style={{alignSelf:'center', padding: 10, flexDirection: 'row',}}>
-            <TouchableOpacity onPress={onSalvarServico}>
+            <TouchableOpacity 
+              onPress={onSalvarServico}>
               <LinearGradient
                 // Button Linear Gradient
                 colors={['#1d817e', '#2fa192', '#50c8cc']}
